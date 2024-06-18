b49445bcacbe0bb4bd581bb096cff23b9ca2e0ba
isabran221 <112730568+isabran221@users.noreply.github.com>
2024-06-17 22:52:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda en linea</title>
+    <title>Westronics</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="css/all.min.css" rel="stylesheet">
@@ -42,7 +42,7 @@ $resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
                             $imagen = "images/productos/$id/principal.jpg";
 
                             if (!file_exists($imagen)) {
-                                $imagen = "images/no-photo.jpg";
+                                $imagen = "images/ruta imagen,jpg";
                             }
                             ?>
                             <a href="details.php?id=<?php echo $row['id']; ?>&token=<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>">
